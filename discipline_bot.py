3f55402161c0e06f0370a477e317f614583e1a09
Zackhardtoname <zack.hardtoname@gmail.com>
2022-09-29 22:14:50 -0700

    Loosen URL check to accommodate new UI
    Thanks to Shin_ for pointing this out!

--- discipline_bot.py
+++ discipline_bot.py
@@ -52,10 +52,10 @@ async def on_ready():
 )
 async def submit(interaction: discord.Interaction, question_number: int, submission_link: str):
     # TODO check with LeetCode API or scrape it
-    if not submission_link.startswith("https://leetcode.com/submissions/detail/"):
+    if "submissions/" not in submission_link:
+        await interaction.user.send_message(f'Your input arguments are "{question_number}", "{submission_link}"')
         await interaction.response.send_message(f'Submission link must look like '
                                                 f'\"https://leetcode.com/submissions/detail/808758751/\"')
-        await interaction.user.send_message(f'Your input arguments are "{question_number}", "{submission_link}"')
         return
 
     # TODO use a proper database
