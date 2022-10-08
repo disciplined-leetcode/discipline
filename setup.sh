bc4c0b97410f1e1d0ce4a2851c0f9b06b559fd0f
Zackhardtoname <zack.hardtoname@gmail.com>
2022-10-08 12:59:04 -0700

    Add setup.sh

---
+++ setup.sh
@@ -0,0 +1,22 @@
+# TODO test it
+mkdir disciplined-leetcode
+cd disciplined-leetcode/
+git clone https://github.com/disciplined-leetcode/discipline.git
+cd discipline/
+echo "alias v=vim" > ~/.bashrc
+# Paste/update special files
+v private.py
+v prod.env
+sudo apt-get update
+sudo apt install python3-pip
+pip3 install pipenv
+pipenv install
+# Paste the following to ~/.bashrc
+# PYTHON_BIN_PATH="$(python3 -m site --user-base)/bin"
+# PATH="$PATH:$PYTHON_BIN_PATH"
+source ~/.bashrc
+v ~/.bashrc
+pipenv install
+pipenv shell
+export DISCIPLINE_MODE=prod && nohup python discipline_bot.py > console.log 2>&1 &
+tail -F console.log
